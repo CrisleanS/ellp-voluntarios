b2648ff06ddcda8bf0cb865fa7c67e79e56dbbdf
CrisleanS <crislean_santos@hotmail.com>
2026-05-12 20:36:14 -0300

    feat: configuração inicial Spring Boot + PostgreSQL (#1)

---
+++ src/test/java/com/ellp/voluntarios/VoluntariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ellp.voluntarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
