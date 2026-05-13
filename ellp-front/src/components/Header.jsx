50911426c722ef9e77bd136136dc8c6c38534768
HeitorPF <heitorpf12.hf@gmail.com>
2026-05-13 04:47:53 -0300

    feat: implementa listagem de voluntários com filtro de status (#7)
    - Criada estrutura de tabela para exibição de voluntários.
    - Implementado consumo da API GET /api/voluntarios.
    - Adicionado toggle para filtrar entre ativos e inativos.

---
+++ ellp-front/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+export function Header() {
+	return (
+		<header>
+			<h1>
+				<span style={{ color: '#0374c8' }}>E</span>
+				<span style={{ color: '#ea7b23' }}>L</span>
+				<span style={{ color: '#0374c8' }}>L</span>
+				<span style={{ color: '#ea7b23' }}>P</span>
+			</h1>
+			<p>Ensino Lúdico de Lógica de Programação</p>
+		</header>
+	);
+}
