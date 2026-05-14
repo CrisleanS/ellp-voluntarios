235b466b81332289c8d45257c566c659f1b8a5e2
CrisleanS <crislean_santos@hotmail.com>
2026-05-14 00:55:35 -0300

    feat: testes unitários e de integração (#5)

---
+++ voluntarios/src/main/java/com/ellp/voluntarios/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.ellp.voluntarios.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
