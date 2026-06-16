508bef201b82dc6f2db43abb6283dfaf522a266a
HeitorPF <heitorpf12.hf@gmail.com>
2026-06-16 04:21:27 -0300

    mudar local logout e pag inicial após login
    Logout no header. Closes #24
    Entra na página inicial direto após login. Closes #25

--- ellp-front/src/components/Header.jsx
+++ ellp-front/src/components/Header.jsx
@@ -4,15 +4,33 @@ import './Header.css'
 export function Header() {
 	const navigate = useNavigate();
 
+	function logout() {
+		localStorage.removeItem('usuario');
+		navigate('/');
+	}
+
 	return (
-		<header onClick={() => navigate('/inicial')}>
-			<h1 className='ellp'>
-				<span style={{ color: '#0374c8' }}>E</span>
-				<span style={{ color: '#ea7b23' }}>L</span>
-				<span style={{ color: '#0374c8' }}>L</span>
-				<span style={{ color: '#ea7b23' }}>P</span>
-			</h1>
-			<p>Ensino Lúdico de Lógica de Programação</p>
+		<header >
+			<div
+				className='logo'
+				onClick={() => navigate('/inicial')}
+			>
+				<h1 className='ellp'>
+					<span style={{ color: '#0374c8' }}>E</span>
+					<span style={{ color: '#ea7b23' }}>L</span>
+					<span style={{ color: '#0374c8' }}>L</span>
+					<span style={{ color: '#ea7b23' }}>P</span>
+				</h1>
+				<p>Ensino Lúdico de Lógica de Programação</p>
+			</div>
+
+
+			<button
+				className='logout-button'
+				onClick={logout}
+			>
+				Sair
+			</button>
 		</header>
 	);
 }
